75a8b8f3859976def4ad89ad6d5b1b4e25f54692
Sardar219 <sardarhossaini2022@gmail.com>
2024-11-04 12:31:15 +0430

    add the updata

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import Logo from "./Logo";
-import Packinglist from "./Packinglist";
-import Stats from "./Stats";
-import Form from "./Form";
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Logo />
@@ -13,5 +8,77 @@ function App() {
     </div>
   );
 }
-
-export default App;
+function Logo() {
+  return (
+    <div className="Logo">
+      <h1>🌴 Far Away 💼</h1>
+    </div>
+  );
+}
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <div className="Form">
+      <form action="" onSubmit={(e) => handleSubmit(e)}>
+        <h4>What do you need for your 🥰 trip</h4>
+        <select name="number" id="number">
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+        <input type="text" placeholder="Item ..." />
+        <input type="submit" value="Add" />
+      </form>
+    </div>
+  );
+}
+function Packinglist() {
+  const initItems = [
+    {
+      id: 1,
+      description: " Passport",
+      quantity: 1,
+      packed: false,
+    },
+    {
+      id: 2,
+      description: " Socks",
+      quantity: 20,
+      packed: true,
+    },
+    {
+      id: 3,
+      description: " Charger",
+      quantity: 2,
+      packed: false,
+    },
+  ];
+  return (
+    <div className="Packinglist">
+      <ul>
+        {initItems.map((item) => (
+          <li key={item.id}>
+            <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+              {item.quantity}
+              {item.description}
+            </span>
+            <button>❌</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Stats() {
+  return (
+    <div className="Stats">
+      <footer>
+        <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      </footer>
+    </div>
+  );
+}
--- src/Logo.js
+++
@@ -1,9 +0,0 @@
-const Logo = () => {
-  return (
-    <div className="Logo">
-      <h1>🌴 Far Away 💼</h1>
-    </div>
-  );
-};
-
-export default Logo;
